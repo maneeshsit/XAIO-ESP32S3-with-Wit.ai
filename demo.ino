a86259d12031136023c2354e01401f05103de626
Maneesh <contact@digx.tech>
2026-07-27 04:46:39 +0530

    demo.ino file for compilation and uploading with Arduino IDE to the ESP32S3 board
    ## Board Packages
    ESP32 Boards package version: 3.3.11
    ESP32-audioI2S library version: 3.4.7
    ##Custom Serial monitor message
    ## Login with Meta account https://wit.ai/ and create an App, go to Management -- Settings of the app and grab the Server Access Token to be used in the code

---
+++ demo.ino
@@ -0,0 +1,61 @@
+// Include WitAITTS Library
+
+#include <WitAITTS.h>
+#include <ESP32I2SAudio.h>
+#include <BackgroundAudio.h>
+
+// Wi-fi and wit.ai credentials
+const char* WIFI_SSID = "TP-Link_E85E";
+const char* WIFI_PASSWORD = "87639001";
+const char* WIT_TOKEN = "V7H5UND6NIR4UZRNGHPNOGUYMJARHZQB";
+
+// Connections to I2S amplifiier
+#define CUSTOM_BCLK 7
+#define CUSTOM_LRC 8
+#define CUSTOM_DIN 9
+
+// Create wit.ai object
+  WitAITTS tts(CUSTOM_BCLK, CUSTOM_LRC, CUSTOM_DIN);
+
+// Setup
+void setup() {
+  
+  // Start Serial Monitor
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("ESP32 with wit.ai demo");
+
+  // Set wit.ai Debug Level
+  tts.setDebugLevel(DEBUG_INFO);
+
+  // Connect to Wi-Fi and wit.ai
+  if (tts.begin(WIFI_SSID, WIFI_PASSWORD, WIT_TOKEN)) {
+    Serial.println("✓ TTS Ready!\n");
+    tts.setVoice("wit$Colin");
+    tts.setStyle("default");
+    tts.setSpeed(100);
+    tts.setPitch(100);
+    tts.setGain(0.3);
+    tts.printConfig();
+    Serial.println("Type any text and press Enter to speak:\n");
+  } else {
+    Serial.println("✗ TTS initialization failed!");
+    Serial.println("Check WiFi credentials and Wit.ai token");
+  }
+}
+
+//Loop
+void loop() {
+  // Check for data
+    tts.loop();
+
+  // Read text from Serial monitor
+  if (Serial.available()) {
+    String text = Serial.readStringUntil('\n');
+    text.trim();
+    if (text.length() > 0) {
+      Serial.println("Speaking: " + text);
+      tts.speak(text);
+    }
+  }
+}
